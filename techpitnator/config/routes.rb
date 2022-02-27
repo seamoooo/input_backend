b91ba4c2688a94bf68c58553a5b9df2f1d1363b8
志茂秀真 <hm.seamo@gmail.com>
2022-02-27 19:11:51 +0900

    create new

--- techpitnator/config/routes.rb
+++ techpitnator/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # ここにgamesコントローラーのnewアクションのルーティングを追加する
+  root 'games#new'
 end
