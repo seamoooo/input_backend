802125e8ae7739249df618839b501d266c96e5f2
志茂秀真 <hm.seamo@gmail.com>
2022-03-03 00:13:04 +0900

    add creste progress

--- techpitnator/app/controllers/progresses_controller.rb
+++ techpitnator/app/controllers/progresses_controller.rb
@@ -4,4 +4,22 @@ class ProgressesController < ApplicationController
     @progress = Progress.new
     @question = Question.first
   end
+
+  def create
+    current_game = Game.find(params[:game_id])
+
+    # Progress.newにしないことで、game_idがバインドされて生成さる
+    progress = current_game.progress.new(create_params)
+    progress.assign_squence
+
+    progress.save!
+
+    redirect_to new_game_progresses_path(current_game)
+  end
+
+  private
+  # ユーザーから入力された値をパラメーターとしてDBに保存する場合はストロングパラメータが必要
+  def create_params
+    params.require(:progress).permit(:question_id, :answer)
+  end
 end
--- techpitnator/app/models/progress.rb
+++ techpitnator/app/models/progress.rb
@@ -1,4 +1,17 @@
 class Progress < ApplicationRecord
   belongs_to :game
   belongs_to :question
+
+  def assign_squence
+    next_sequence = 1
+    if game.present?
+      all_progress = game.progress
+      if all_progress.count > 0
+        # maximum　カラムの最大値を計算
+        next_sequence = all_progress.maximum(:sequence) + 1
+      end
+    end
+    self.sequence = next_sequence
+  end
+
 end
