d1a1ab7c83b32fcae2c78d6072455aa6fd968e1b
志茂秀真 <hm.seamo@gmail.com>
2022-03-03 22:13:05 +0900

    add differnt question

--- techpitnator/app/models/question.rb
+++ techpitnator/app/models/question.rb
@@ -1,3 +1,8 @@
 class Question < ApplicationRecord
   has_many :progress
+
+  def self.next_question(game)
+    anssere_question_ids = game.progress.pluck(:question_id)
+    Question.where.not(id: anssere_question_ids).shuffle.take(1).first
+  end
 end
